36a8cfac3410b6f6bc73a029d418924f494c98a5
vkmuc <vkmuccharla@gmail.com>
2020-06-12 17:39:54 +0530

    @BeforeClass @AfterClass @Test @Ignore

--- 101-JUnit-Test1/src/main/java/com/vj/Addition.java
+++ 101-JUnit-Test1/src/main/java/com/vj/Addition.java
@@ -5,5 +5,11 @@ public class Addition {
 	public int add(int x, int y) {
 		return x+y;
 	}
-	
+
+	public int substract(int x, int y) {
+		if(x>y || x==y)
+			return x-y;
+		else
+			return -(y-x);
+	}
 }
