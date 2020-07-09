50f517711354b66a6ad97cb268f8d3463555a32d
vkmuc <vkmuccharla@gmail.com>
2020-07-09 23:48:41 +0530

    MP1

--- 104-JRTP-MP1_ContactsBook-CRUD-WebApp/src/main/java/com/vj/controller/ViewAllContactsController.java
+++ 104-JRTP-MP1_ContactsBook-CRUD-WebApp/src/main/java/com/vj/controller/ViewAllContactsController.java
@@ -1,7 +1,5 @@
 package com.vj.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.vj.entities.ContactEntity;
 import com.vj.model.Contact;
 import com.vj.service.ContactService;
 
